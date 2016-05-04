2f27d00d29c6049e6272fdd50aea89c670b4936f
yonghui.luo <yonghui.luo@duomi.com>
2016-05-04 10:57:30 +0800

    Initial commit

---
+++ app/src/main/java/com/tiny/smallhost/Application.java
@@ -0,0 +1,20 @@
+package com.tiny.smallhost;
+
+import net.wequick.small.Small;
+
+/**
+ * Function：
+ * <p/>
+ * Created by ${Tiny}
+ * on 2016/5/2.
+ */
+public class Application extends android.app.Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Small.setBaseUri("http://m.wequick.net/demo/");
+
+        Small.preSetUp(this);
+    }
+}
